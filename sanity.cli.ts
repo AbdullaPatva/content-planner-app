88eca46aa84d66ede703b4b0da518538e4354e56
Abdulla Patva <abdulla.patva@multidots.com>
2025-08-04 16:50:03 +0530

    Fix Sanity app configuration and deploy to Sanity.io dashboard

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,10 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   app: {
+    id: 'eoxcb2jkvstsjz53b4nztwar',
     organizationId: 'oopxIC4jC',
     entry: './src/App.tsx',
+    projectId: 'o7xwtv7a',
+    dataset: 'production',
   },
 })
